a9975ee81adffc83d377c22304186b3ff2102ff4
Nastia Andreichuk <jimmlie18@gmail.com>
2019-11-08 21:12:20 +0200

    Add project files.

---
+++ NewsAggregator/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NewsAggregator.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
