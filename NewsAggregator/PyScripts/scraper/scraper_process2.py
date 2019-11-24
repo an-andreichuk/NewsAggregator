a3c7c536502ed5524da77b5c4997674bc9add914
Volodymyr Pankin <vovan3819@gmail.com>
2019-11-24 02:38:10 +0200

    Add scraper for gordon.ua

--- NewsAggregator/PyScripts/scraper/scraper_process2.py
+++ NewsAggregator/PyScripts/scraper/scraper_process2.py
@@ -14,20 +14,20 @@ from scrapy.utils.project import get_project_settings
 
 news_urls = []
 checked_news = []
-domain = "https://www.pravda.com.ua"
+domain = "https://gordonua.com/ukr/"
 
 client = MongoClient(
     'mongodb+srv://writer:writer-pass-123@cluster0-kgt9l.azure.mongodb.net/test?retryWrites=true&w=majority')
 db = client['News']
-collection = db['pravda.com.ua']
+collection = db['gordon.ua']
 
 
-class QuotesSpider(scrapy.Spider):
-    name = "quotes"
+class GordonSpider(scrapy.Spider):
+    name = "gordon"
 
     def start_requests(self):
         self.urls = [
-            'https://www.pravda.com.ua'
+            'https://gordonua.com/ukr/'
         ]
         not_scraped = list(set(news_urls) - set(checked_news))
         for url in not_scraped + self.urls:
@@ -35,7 +35,7 @@ class QuotesSpider(scrapy.Spider):
 
     def parse(self, response):
         if response.request.url in self.urls:
-            for x in response.xpath('//div[@class="article__title"]//a/@href').extract():
+            for x in response.xpath('//div[@class="tab-content"]//a/@href').extract():
                 if len(x) > 0 and x[0] == '/':
                     x = domain + x
                 if x.startswith(domain):
@@ -47,14 +47,14 @@ class QuotesSpider(scrapy.Spider):
             Tags = []
             TimeSourcePublished = datetime.utcnow() + timedelta(hours=2)
 
-            if len(response.xpath('//h1[@class="post_news__title"]').extract()) != 0:
-                Title = response.xpath('//h1[@class="post_news__title"]//text()').extract()[0]
+            if len(response.xpath('//h1[@class="a_head flipboard-title"]').extract()) != 0:
+                Title = response.xpath('//h1[@class="a_head flipboard-title"]/text()').extract()[0]
             else:
                 return
-            text_parts = response.xpath('//div[@class="post_news__text"]/p//text()').extract()
+            text_parts = response.xpath('//div[@class="a_body"]/p/text()').extract()
             normalized_article_strings = utils.normalize(text_parts)
             Text = "\n".join(normalized_article_strings)
-            for x in response.xpath('//span[@class="post__tags__item"]/a/text()').extract():
+            for x in response.xpath('//div[@class="tags flipboard-endArticle"]/a/text()').extract():
                 Tags.append(x)
 
             collection.insert_one({'Title': Title,
@@ -65,9 +65,8 @@ class QuotesSpider(scrapy.Spider):
 
             checked_news.append(response.request.url)
 
-
-process = CrawlerProcess(get_project_settings())
 counter = 0
+process = CrawlerProcess(get_project_settings())
 
 print(checked_news)
 
@@ -84,5 +83,5 @@ def _crawl(result, spider):
     return deferred
 
 
-_crawl(None, QuotesSpider)
+_crawl(None, GordonSpider)
 process.start()
